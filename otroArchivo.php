cc29cc9bd89c03a977783c0cccc3e6367d48e989
David Carrillo <dcarrillo@istta.edu.pe>
2020-12-10 09:16:20 -0500

    Mi primer respaldo del Proyecto

---
+++ otroArchivo.php
@@ -0,0 +1,8 @@
+<?php
+    echo "Hola soy otroArchivo.php";
+    echo "Soy otra línea modificada a las 9:06";
+    for ($i=0; $i < 10; $i++) { 
+        echo $i+"-<br>";
+    }
+    echo "Soy otra línea modificada a las 9:10";
+?>
